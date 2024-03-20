584fa78253be9c32f1a4009eb4d8252a2013153d
Harry Qiu <hqiu678@outlook.com>
2024-03-20 11:54:33 +0000

    make dummy gain model

--- scepter/pfd.py
+++ scepter/pfd.py
@@ -6,6 +6,8 @@ pfd.py
 
 Call functions for power flux density calculation
 
+At the moment I will use a dummy isotropic antenna gain with cysgp4 and pycraf 
+
 Author: Harry Qiu <hqiu678@outlook.com>
 """
 
@@ -17,31 +19,88 @@ from pycraf import conversions as cnv
 from pycraf import protection, antenna, geometry
 from astropy import units as u, constants as const
 
-def sat_gain_func(sat_obs_az, sat_obs_el):
-    # Use 0 dBi antenna for the simulations
-    
-    sat_obs_az, sat_obs_el = np.broadcast_arrays(sat_obs_az, sat_obs_el)
-    G_tx = np.zeros(sat_obs_az.shape, dtype=np.float64) * cnv.dBi
+def dummy_gain(sat_obs_az,sat_obs_el,d_tx,freq,tp_az,tp_el):
+    '''
+    Description: Retrieves gain from basic gain pattern function with angular separation to pointing considered
+
+    Parameters:
+    sat_obs_az: float
+        azimuth angle of the satellite
+    sat_obs_el: float
+        elevation angle of the satellite
+    d_tx: float
+        diameter of the transmitter telescope
+    freq: float
+        frequency of the signal
+    tp_az: float
+        azimuth angle of the telescope pointing
+    tp_el: float
+        elevation angle of the telescope pointing
+
+    Returns:
+    G_tx: float
+        transmitter gain
+    '''
+    gmax=antenna.fl_G_max_from_size(d_tx,freq)
+    hpbw=antenna.fl_hpbw_from_size(d_tx,freq)  
+    ### calculate angular separation of satellite to telescope pointing
+    phi=geometry.angular_separation(tp_az,tp_el,sat_obs_az,sat_obs_el) 
+    flpattern=antenna.fl_pattern(phi,diameter=d_tx,frequency=freq,G_max=gmax)
+
     return G_tx
 
-def FPSL(freq, dist):
-    # Calculate the free space path loss
-    FSPL = cnv.free_space_loss(
-        dist * u.km, freq
-        ).to(cnv.dB)
-    return FSPL
 
 def grx(ang_sep,d_rx,freq,eta_a_rx):
-    # this function retrieves the antenna gain at certain angle
+    '''
+    Description: This function calculates the receiver gain
+
+    Parameters:
+    ang_sep: float
+        angular separation between observing direction and telescope pointing
+    d_rx: float
+        diameter of the receiver telescope
+    freq: float
+        frequency of the signal
+    eta_a_rx: float
+        aperture efficiency of the receiver telescope
+    '''
+    
     G_rx = antenna.ras_pattern(
         ang_sep, d_rx, const.c / freq, eta_a_rx
         )
     return G_rx
 
+
+def ptx(p_tx_carrier,carrier_bandwidth,duty_cycle,ras_bandwidth):
+    # Calculate the transmitted power
+    p_tx_nu_peak = (
+    p_tx_carrier.physical / carrier_bandwidth
+    ).to(u.W / u.Hz)
+    p_tx_nu = p_tx_nu_peak * duty_cycle
+    p_tx = p_tx_nu.to(u.W / u.Hz) * ras_bandwidth
+    p_tx = p_tx_carrier
+    return p_tx
+
 def prx(freq,sat_obs_dist,sat_obs_az,sat_obs_el):
-    # Calculate the received power flux density
-    FSPL = FPSL(freq, sat_obs_dist)
-    G_tx = sat_gain_func(
+    '''
+    Description: This function calculates the received power, astropy units needed for all inputs
+
+    Parameters:
+    freq: float
+        frequency of the signal
+    sat_obs_dist: float
+        distance between the satellite and the observer 
+    sat_obs_az: float
+        azimuth angle of the satellite
+    sat_obs_el: float
+        elevation angle of the satellite
+    
+    Returns:
+    p_rx: float
+        received power (Watts)
+    '''
+    FSPL = cnv.free_space_lost(sat_obs_dist,freq) ### calculate free space path loss
+    G_tx = sat_gain_func(  ### calculate transmitter gain
         sat_obs_az * u.deg,
         sat_obs_el * u.deg,
         )
